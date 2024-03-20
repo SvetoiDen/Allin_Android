6e02a2f48ead01c33e40ec3c6b62aa517afdfdae
Svetoi_Den <geymerworld123@gmail.com>
2024-03-20 19:10:01 +0300

    updrade install!

--- app/src/main/java/com/example/allin_android/clock.java
+++ app/src/main/java/com/example/allin_android/clock.java
@@ -3,15 +3,37 @@ package com.example.allin_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class clock extends AppCompatActivity {
+
+    TextView text_cloak;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clock);
+
+        final TextView cloak_text = findViewById(R.id.clock_time);
+        final Handler handler = new Handler();
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                Calendar calendar = Calendar.getInstance();
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
+                String currentTime = simpleDateFormat.format(calendar.getTime());
+                cloak_text.setText(currentTime);
+                handler.postDelayed(this, 1000);
+            }
+        };
+        handler.post(runnable);
     }
 
     public void Back(View v) {
