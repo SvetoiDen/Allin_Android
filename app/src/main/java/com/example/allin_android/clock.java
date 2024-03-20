19189a89ca190db4e9e49b4907f7e3ea4f29d814
Svetoi_Den <geymerworld123@gmail.com>
2024-03-20 18:35:25 +0300

    updrade install!

---
+++ app/src/main/java/com/example/allin_android/clock.java
@@ -0,0 +1,14 @@
+package com.example.allin_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class clock extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clock);
+    }
+}
